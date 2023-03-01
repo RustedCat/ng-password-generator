3a4870f1a95acccac49cd2b1963c58af7200983e
Djihad Boukara <djihad.boukara@proton.me>
2023-03-01 18:38:12 +0100

    refactoring to improve readability

--- src/app/services/password-generator.service.ts
+++ src/app/services/password-generator.service.ts
@@ -7,63 +7,22 @@ import { IPassword } from '../models/password.model';
 export class PasswordGeneratorService {
   generatedPassword: EventEmitter<string> = new EventEmitter();
 
-  private ranges = {
-    lower_start: 'a'.charCodeAt(0),
-    lower_range: 'z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1,
-    upper_start: 'A'.charCodeAt(0),
-    upper_range: 'Z'.charCodeAt(0) - 'A'.charCodeAt(0) + 1,
-    nums_start: '0'.charCodeAt(0),
-    nums_range: '9'.charCodeAt(0) - '0'.charCodeAt(0) + 1,
-  };
-
-  private lower_chars = this.range(
-    this.ranges.lower_range,
-    this.ranges.lower_start
-  );
-
-  private upper_chars = this.range(
-    this.ranges.lower_range,
-    this.ranges.lower_start
-  );
-
-  private nums_chars = this.range(
-    this.ranges.nums_range,
-    this.ranges.nums_start
-  );
-
-  private special_chars = [33, 35, 36, 37, 38, 42, 64, 94];
-
-  constructor() {}
-
-  private range(size: number, startAt = 0) {
-    return [...Array(size).keys()].map((i) => i + startAt);
-  }
+  private lower_chars = 'abcdefghijklmnopqrstuvwxyz';
+  private upper_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private nums_chars = '0123456789';
+  private special_chars = '!@#$%^&*';
 
   generatePassword(requirements: IPassword) {
-    let allowed: number[] = [];
-    if (requirements.hasUppercase) {
-      allowed = [...this.upper_chars];
-    }
-
-    if (requirements.hasLowercase) {
-      allowed = [...allowed, ...this.lower_chars];
-    }
-
-    if (requirements.hasNumbers) {
-      allowed = [...allowed, ...this.nums_chars];
-    }
-
-    if (requirements.hasSpecial) {
-      allowed = [...allowed, ...this.special_chars];
-    }
+    let allowed = '';
+    if (requirements.hasUppercase) allowed += this.upper_chars;
+    if (requirements.hasLowercase) allowed += this.lower_chars;
+    if (requirements.hasNumbers) allowed += this.nums_chars;
+    if (requirements.hasSpecial) allowed += this.special_chars;
 
     let password = '';
 
-    for (let i = 0; i < requirements.length; ++i) {
-      password += String.fromCharCode(
-        allowed[Math.floor(Math.random() * allowed.length)]
-      );
-    }
+    for (let i = 0; i < requirements.length; ++i)
+      password += allowed[Math.floor(Math.random() * allowed.length)];
 
     this.generatedPassword.emit(password);
   }
